93f9ca992e81896f98e25f29db494b741943ff57
Julia Gorunova <y.gorunova@lucky-labs.com>
2016-06-21 19:37:21 +0300

    sizer added

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,7 @@ module.exports = function(grunt) {
                 src: [
                     "src/js/init.js",
                     "src/js/paginator.js",
+                    "src/js/sizer.js",
                     "src/js/cells.js",
                     "src/js/body.js",
                     "src/js/header.js",
--- dist/marionette.grid.js
+++ dist/marionette.grid.js
@@ -173,6 +173,59 @@ var PaginatorView = MaGrid.PaginatorView = Marionette.CollectionView.extend({
     }
 });
 
+
+var SizerView = MaGrid.SizerView = Marionette.CollectionView.extend({
+    tagName: 'ul',
+    childViewEventPrefix:'sizer',
+    childView: Marionette.ItemView.extend({
+        tagName: 'li',
+        template: _.template('<a href="#" title="<%= page_size %> items per page"><%= page_size %></a>'),
+        onRender: function() {
+            this.$el.removeClass(this.getOption('currentPageClass'));
+            if(this.model.get('is_current')) {
+                this.$el.addClass(this.getOption('currentPageClass'));
+            }
+        },
+        modelEvents: {
+            'change': 'render'
+        },
+        events: {
+            'click a': 'on_click'
+        },
+        on_click: function(e) {
+            e.preventDefault();
+            this.trigger('size:click');
+        }
+    }),
+    childViewOptions: function() {
+        return {
+            currentPageClass: this.getOption('currentPageClass')
+        }
+    },
+    initialize: function() {
+        this.collection = new Backbone.Collection([], {
+            model: Backbone.Model.extend({
+                defaults: {
+                    page_size: null,
+                    is_current: false
+                }
+            })
+        });
+        var page_sizes = this.getOption('pageSizes');
+        var current_page_size = this.getOption('bindedCollection').state.pageSize;
+        for(var i in page_sizes) {
+            this.collection.add({
+                is_current: (page_sizes[i] ==  current_page_size),
+                page_size: page_sizes[i]
+            });
+        }
+    },
+    onRender: function() {
+        this.$el.addClass(this.getOption('sizerClassName'));
+    },
+
+});
+
 var CellView = MaGrid.CellView = Marionette.ItemView.extend({
     tagName: 'td',
     template: _.template('<div><%= display_value  %></div>'),
